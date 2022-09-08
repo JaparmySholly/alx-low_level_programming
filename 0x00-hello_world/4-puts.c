85a8161de9d605c594291764ca3c583066848d73
JaparmySholly <japarmysholly@gmail.com>
2022-09-08 14:42:35 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Betty
  *
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+{
+	puts("\"Programming is like buiding a multilingual puzzle");
+	return (0);
 }
