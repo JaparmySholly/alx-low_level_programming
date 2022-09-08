bb723583931fab3f1ebe52fd6b30b28e40332599
JaparmySholly <japarmysholly@gmail.com>
2022-09-08 13:07:14 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- *  * Description: main - Prints out a string to stdout.
- *   * Return: 0 if success.
- *    */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
