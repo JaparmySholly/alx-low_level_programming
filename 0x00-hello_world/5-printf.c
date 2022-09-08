1b37aaf21d8c59c5438698e88b909f2be92b0e5d
JaparmySholly <japarmysholly@gmail.com>
2022-09-08 15:13:23 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
- * * main - Entry point
- * *
- * * Return: Always 0 (Success)
- * */
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
 }
