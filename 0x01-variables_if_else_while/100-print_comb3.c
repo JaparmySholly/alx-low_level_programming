a31475571f7fd371a3829373639f27504f151fa3
JaparmySholly <japarmysholly@gmail.com>
2022-09-09 15:42:55 -0700

    C

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,26 @@
 
 int main(void)
 {
-	int digit1, digit2;
-
-	for (digit1 = 0; digit1 < 9; digit1++)
+	int c, i;
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+		for (i = '0'; i <= '9'; i++)
 
-			if (digit1 == 8 && digit2 == 9)
-				continue;
+			{
+				if (c < i)
+				{
+					putchar(c);
+					putchar(i);
 
-			putchar(',');
-			putchar(' ');
-		}
+					if (c != '8' || (c == '8' && i != '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 	}
 
 	putchar('\n');
-
 	return (0);
 }
