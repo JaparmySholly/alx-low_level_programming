4fb88a03e354f0e46a42d8ba8bbe70cbd6ea1056
JaparmySholly <japarmysholly@gmail.com>
2022-09-13 09:53:24 -0700

    Main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
  * main - entry point
- * Return: 0
+ * Return: nothing, return void
+ *
  */
 
-int main(void)
+int main(0)
 {
-	char *sh = "main";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n')
 
 	return (0);
 }
