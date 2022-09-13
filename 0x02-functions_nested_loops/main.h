adad069a4f7bb0d6a521fa5d86b3d1917b3414fc
JaparmySholly <japarmysholly@gmail.com>
2022-09-13 10:24:19 -0700

    Main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints the string "main.h" from a character array.
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	chr c[] = "main.h";
+	int i;
+
+	for (i=0; i<9; i++)
+	{
+		_putchar(c[i]);
+	}
+		_putchar('\n');
+		return (0);
+	}
