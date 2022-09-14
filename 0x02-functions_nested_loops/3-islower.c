a1c1e20278fc3e7101a712ff4c26f260c8948f55
JaparmySholly <japarmysholly@gmail.com>
2022-09-14 04:31:36 -0700

    Islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 /**
- * islower - checks for lower case of argument
+ * _islower - checks for lower case of argument
  * @c: Th value to be checked
  * Description: The functions checks and return value base* on check the values
  * Return: 1 if int c is lowercase, 0 if otherwise
