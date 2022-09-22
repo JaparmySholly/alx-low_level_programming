6e2903c9f513f798820d7fefcd4abfdb3a7ff9f9
JaparmySholly <japarmysholly@gmail.com>
2022-09-22 14:47:22 -0700

    fixed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,6 +13,7 @@ int main(void)
 	/**
 	 * write your line of code here...
 	 * Remember:
+	 * main: dont't know what to type!
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
