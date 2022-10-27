b3ada8acc9f0d2e573a4175a709e98b858cb75c0
JaparmySholly <japarmysholly@gmail.com>
2022-10-27 11:35:52 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,46 +1,42 @@
 #include "main.h"
 
 /**
- * _pow - calculates (base ^ power)
- * @base: base of the exponent
- * @power: power of the exponent
- * Return: value of (base ^ power)
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
  */
-unsigned long int _pow(unsigned int base, unsigned int power)
-{
-	unsigned long int num;
-	unsigned int i;
 
-	num = 1;
-	for (i = 1; i <= power; i++)
-		num *= base;
-	return (num);
-}
-
-/**
- * print_binary - prints a number in binary notation
- * @n: number to print
- * Return: void
- */
 void print_binary(unsigned long int n)
-{
-	unsigned long int divisor, check;
-	char flag;
 
-	flag = 0;
-	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
-	while (divisor != 0)
+{
+	unsigned long int revnum;
+	unsigned long int count;
+
+	revnum = count = 0;
+	if (n == 0)
+		_putchar('0');
+
+	while (n > 0)
 	{
-		check = n & divisor;
-		if (check == divisor)
-		{
-			flag = 1;
-			_putchar('1');
-		}
-		else if (flag == 1 || divisor == 1)
+		revnum = revnum << 1;
+		revnum += n & 1;
+		count++;
+		n = n >> 1;
+	}
+
+	while (revnum > 0)
+	{
+		_putchar((revnum & 1) + '0');
+		revnum = revnum >> 1;
+		count--;
+	}
+
+	if (count > 0)
+	{
+		while (count != 0)
 		{
 			_putchar('0');
+			count--;
 		}
-		divisor >>= 1;
+	}
 
 }
