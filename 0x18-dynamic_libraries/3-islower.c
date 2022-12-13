315e1b6067d929c3fedfb123daf777b6dd0ee834
JaparmySholly <japarmySholly@gmail.com>
2022-12-13 17:19:08 +0000

    Update

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - checks for lower case of argument
+ * @c: Th value to be checked
+ * Description: The functions checks and return value base* on check the values
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ *
+ */
+
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
